6bf4893ebf9c35554664d9152db0a52caf1de4f9
Sutrisno Efendi <kangfend@gmail.com>
2018-03-14 17:38:13 +0700

    Limit result using maximum arguemt

--- ig_scraper/scraper.py
+++ ig_scraper/scraper.py
@@ -8,22 +8,22 @@ class IGScraper:
         self.hashtag = hashtag
         self.items = []
 
-    def scrape_hashtag(self, end_cursor=''):
+    def scrape_hashtag(self, end_cursor='', maximum=10):
         try:
-            response = requests.get(QUERY_HASHTAG.format(self.hashtag, end_cursor)).json()
+            response = requests.get(QUERY_HASHTAG.format(
+                self.hashtag, end_cursor)).json()
             data = response['data']['hashtag']
         except:
             data = []
 
         if data:
-            end_cursor = data['edge_hashtag_to_media']['page_info']['end_cursor']
             for item in data['edge_hashtag_to_media']['edges']:
                 node = item['node']
                 if node['edge_media_to_caption']['edges']:
                     caption = node['edge_media_to_caption']['edges'][0]['node']['text']
                 else:
                     caption = None
-                data = {
+                item = {
                     'is_video': node['is_video'],
                     'caption': caption,
                     'display_url': node['display_url'],
@@ -32,8 +32,9 @@ class IGScraper:
                     'shortcode': node['shortcode'],
                     'taken_at_timestamp': node['taken_at_timestamp']
                 }
-                if data not in self.items and data['is_video'] is False and caption:
-                    self.items.append(data)
-            if end_cursor:
-                self.scrape_hashtag(end_cursor=end_cursor)
+                if item not in self.items and len(self.items) < maximum:
+                    self.items.append(item)
+            end_cursor = data['edge_hashtag_to_media']['page_info']['end_cursor']
+            if end_cursor and len(self.items) < maximum:
+                self.scrape_hashtag(end_cursor=end_cursor, maximum=maximum)
         return self.items
