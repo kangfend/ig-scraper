038bcd1cebebc0f0746c33b4954f98e489c5fe7f
Sutrisno Efendi <kangfend@gmail.com>
2018-04-20 15:35:17 +0700

    Fix maximum argument

--- ig_scraper/constants.py
+++ ig_scraper/constants.py
@@ -1,7 +1,7 @@
 BASE_URL = 'https://www.instagram.com/'
 
 # QUERY hashtag
-QUERY_HASHTAG = BASE_URL + 'graphql/query/?query_id=17882293912014529&tag_name={0}&first=10&after={1}'
+QUERY_HASHTAG = BASE_URL + 'graphql/query/?query_id=17882293912014529&tag_name={0}&first={1}&after={2}'
 
 # User agent
 CHROME_WIN_UA = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
