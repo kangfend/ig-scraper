aa378d680555b41c0eb60b2c294179dc812110eb
Sutrisno <kangfend@gmail.com>
2018-12-16 20:02:41 +0700

    Import variable name instead of *

--- ig_scraper/scraper.py
+++ ig_scraper/scraper.py
@@ -3,13 +3,13 @@ import json
 import requests
 import sys
 
-from .constants import *  # noqa
+from .constants import (CHROME_WIN_UA, BASE_URL, QUERY_HASHTAG,
+                        QUERY_HASHTAG_VARS, MEDIA_URL)
 
 
 class IGScraper:
     def __init__(self):
         self.items = []
-
         self.session = requests.Session()
         self.session.headers = {'user-agent': CHROME_WIN_UA}
         self.session.cookies.set('ig_pr', '1')
@@ -31,28 +31,23 @@ class IGScraper:
         data = rhx_gis + ":" + params
         if sys.version_info.major >= 3:
             return hashlib.md5(data.encode('utf-8')).hexdigest()
-        else:
-            return hashlib.md5(data).hexdigest()
+        return hashlib.md5(data).hexdigest()
 
     def update_ig_gis_header(self, params):
         self.rhx_gis = self.get_shared_data()['rhx_gis']
         self.session.headers.update({
-            'x-instagram-gis': self.get_ig_gis(
-                self.rhx_gis,
-                params
-            )
+            'x-instagram-gis': self.get_ig_gis(self.rhx_gis, params)
         })
 
-    def scrape_hashtag(self, hashtag, end_cursor='', maximum=10, first=10, initial=True,
-                       detail=False):
+    def scrape_hashtag(self, hashtag, end_cursor='', maximum=10, first=10,
+                       initial=True, detail=False):
         if initial:
             self.items = []
 
         try:
             params = QUERY_HASHTAG_VARS.format(hashtag, end_cursor)
             self.update_ig_gis_header(params)
-            response = self.session.get(QUERY_HASHTAG.format(params))
-            response = response.json()
+            response = self.session.get(QUERY_HASHTAG.format(params)).json()
             data = response['data']['hashtag']
         except Exception:
             data = []
@@ -60,10 +55,9 @@ class IGScraper:
         if data:
             for item in data['edge_hashtag_to_media']['edges']:
                 node = item['node']
+                caption = None
                 if node['edge_media_to_caption']['edges']:
                     caption = node['edge_media_to_caption']['edges'][0]['node']['text']
-                else:
-                    caption = None
 
                 if any([detail, node['is_video']]):
                     try:
@@ -96,10 +90,8 @@ class IGScraper:
                 if item not in self.items and len(self.items) < maximum:
                     self.items.append(item)
 
-            end_cursor = data['edge_hashtag_to_media']['page_info']['end_cursor']
-
+            end_cursor = data['edge_hashtag_to_media']['page_info']['end_cursor']  # noqa
             if end_cursor and len(self.items) < maximum:
                 self.scrape_hashtag(end_cursor=end_cursor, detail=detail,
                                     maximum=maximum, initial=False)
-
         return self.items
